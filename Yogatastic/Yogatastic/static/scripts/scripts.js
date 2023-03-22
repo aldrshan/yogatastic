8f98f8730c322a793a16d1829ce05fa9d3357b5c
shnaldridge <shan.d.aldridge@gmail.com>
2023-03-22 11:17:39 -0400

    workouts completed. Pictures dont align.

--- Yogatastic/Yogatastic/static/scripts/scripts.js
+++ Yogatastic/Yogatastic/static/scripts/scripts.js
@@ -1,29 +1,88 @@
 const yogaPoses = {
-    workout1: [
+    day1: [
         { name: "Child's Pose", image: "static/img/childs_pose.jpg" },
         { name: "Cat-Cow Pose", image: "static/img/cat_cow_pose.jpg" },
-        // Add more poses for workout1
+        { name: "Downward-Facing Dog", image: "static/img/downward_facing_dog.jpg" },
+        { name: "Three-Legged Dog (Right)", image: "static/img/three_legged_dog_right.jpg" },
+        { name: "Low Lunge (Right)", image: "static/img/low_lunge_right.jpg" },
+        { name: "Three-Legged Dog (Left)", image: "static/img/three_legged_dog_left.jpg" },
+        { name: "Low Lunge (Left)", image: "static/img/low_lunge_left.jpg" },
+        { name: "Mountain Pose", image: "static/img/mountain_pose.jpg" },
+        { name: "Standing Forward Bend", image: "static/img/standing_forward_bend.jpg" },
+        { name: "Halfway Lift", image: "static/img/halfway_lift.jpg" },
     ],
-    workout2: [
-        { name: "Pose1", image: "static/img/pose1.jpg" },
-        { name: "Pose2", image: "static/img/pose2.jpg" },
-        // Add more poses for workout2
+    day2: [
+        { name: "Easy Pose", image: "static/img/easy_pose.jpg" },
+        { name: "Twist (Right)", image: "static/img/twist_right.jpg" },
+        { name: "Twist (Left)", image: "static/img/twist_left.jpg" },
+        { name: "Tabletop Pose", image: "static/img/tabletop_pose.jpg" },
+        { name: "Thread the Needle (Right)", image: "static/img/thread_needle_right.jpg" },
+        { name: "Thread the Needle (Left)", image: "static/img/thread_needle_left.jpg" },
+        { name: "Puppy Pose", image: "static/img/puppy_pose.jpg" },
+        { name: "Cobra Pose", image: "static/img/cobra_pose.jpg" },
+        { name: "Child's Pose", image: "static/img/childs_pose.jpg" },
+        { name: "Corpse Pose", image: "static/img/corpse_pose.jpg" },
     ],
-    workout3: [
-        { name: "Pose1", image: "static/img/pose1.jpg" },
-        { name: "Pose2", image: "static/img/pose2.jpg" },
-        // Add more poses for workout2
+    day3: [
+        { name: "Seated Forward Bend", image: "static/img/seated_forward_bend.jpg" },
+        { name: "Seated Wide-Legged Forward Bend", image: "static/img/seated_wide_legged_forward_bend.jpg" },
+        { name: "Bound Angle Pose", image: "static/img/bound_angle_pose.jpg" },
+        { name: "Bridge Pose", image: "static/img/bridge_pose.jpg" },
+        { name: "Happy Baby", image: "static/img/happy_baby.jpg" },
+        { name: "Supine Twist (Right)", image: "static/img/supine_twist_right.jpg" },
+        { name: "Supine Twist (Left)", image: "static/img/supine_twist_left.jpg" },
+        { name: "Reclined Butterfly", image: "static/img/reclined_butterfly.jpg" },
+        { name: "Corpse Pose", image: "static/img/corpse_pose.jpg" },
     ],
-    workout4: [
-        { name: "Pose1", image: "static/img/pose1.jpg" },
-        { name: "Pose2", image: "static/img/pose2.jpg" },
-        // Add more poses for workout2
+    day4: [
+        { name: "Mountain Pose", image: "static/img/mountain_pose.jpg" },
+        { name: "Chair Pose", image: "static/img/chair_pose.jpg" },
+        { name: "Standing Forward Bend", image: "static/img/standing_forward_bend.jpg" },
+        { name: "Halfway Lift", image: "static/img/halfway_lift.jpg" },
+        { name: "High Plank", image: "static/img/high_plank.jpg" },
+        { name: "Low Plank", image: "static/img/low_plank.jpg" },
+        { name: "Upward-Facing Dog", image: "static/img/upward_facing_dog.jpg" },
+        { name: "Downward-Facing Dog", image: "static/img/downward_facing_dog.jpg" },
+        { name: "Warrior I (Right)", image: "static/img/warrior_i_right.jpg" },
+        { name: "Warrior I (Left)", image: "static/img/warrior_i_left.jpg" },
     ],
-    workout5: [
-        { name: "Pose1", image: "static/img/pose1.jpg" },
-        { name: "Pose2", image: "static/img/pose2.jpg" },
-        // Add more poses for workout2
+    day5: [
+        { name: "Warrior II (Right)", image: "static/img/warrior_ii_right.jpg" },
+        { name: "Warrior II (Left)", image: "static/img/warrior_ii_left.jpg" },
+        { name: "Extended Side Angle (Right)", image: "static/img/extended_side_angle_right.jpg" },
+        { name: "Extended Side Angle (Left)", image: "static/img/extended_side_angle_left.jpg" },
+        { name: "Reverse Warrior (Right)", image: "static/img/reverse_warrior_right.jpg" },
+        { name: "Reverse Warrior (Left)", image: "static/img/reverse_warrior_left.jpg" },
+        { name: "Triangle Pose (Right)", image: "static/img/triangle_pose_right.jpg" },
+        { name: "Triangle Pose (Left)", image: "static/img/triangle_pose_left.jpg" },
+        { name: "Wide-Legged Forward Bend", image: "static/img/wide_legged_forward_bend.jpg" },
+        { name: "Pyramid Pose (Right)", image: "static/img/pyramid_pose_right.jpg" },
     ],
+    day6: [
+        { name: "Pyramid Pose (Left)", image: "static/img/pyramid_pose_left.jpg" },
+        { name: "Tree Pose (Right)", image: "static/img/tree_pose_right.jpg" },
+        { name: "Tree Pose (Left)", image: "static/img/tree_pose_left.jpg" },
+        { name: "Eagle Pose (Right)", image: "static/img/eagle_pose_right.jpg" },
+        { name: "Eagle Pose (Left)", image: "static/img/eagle_pose_left.jpg" },
+        { name: "Dancer Pose (Right)", image: "static/img/dancer_pose_right.jpg" },
+        { name: "Dancer Pose (Left)", image: "static/img/dancer_pose_left.jpg" },
+        { name: "Half Camel Pose (Right)", image: "static/img/half_camel_pose_right.jpg" },
+        { name: "Half Camel Pose (Left)", image: "static/img/half_camel_pose_left.jpg" },
+        { name: "Child's Pose", image: "static/img/childs_pose.jpg" },
+    ],
+    day7: [
+        { name: "Cat-Cow Pose", image: "static/img/cat_cow_pose.jpg" },
+        { name: "Downward-Facing Dog", image: "static/img/downward_facing_dog.jpg" },
+        { name: "Pigeon Pose (Right)", image: "static/img/pigeon_pose_right.jpg" },
+        { name: "Pigeon Pose (Left)", image: "static/img/pigeon_pose_left.jpg" },
+        { name: "Seated Forward Bend", image: "static/img/seated_forward_bend.jpg" },
+        { name: "Seated Twist (Right)", image: "static/img/seated_twist_right.jpg" },
+        { name: "Seated Twist (Left)", image: "static/img/seated_twist_left.jpg" },
+        { name: "Boat Pose", image: "static/img/boat_pose.jpg" },
+        { name: "Plow Pose", image: "static/img/plow_pose.jpg" },
+        { name: "Corpse Pose", image: "static/img/corpse_pose.jpg" },
+    ],
+
 };
 
 const timerElement = document.getElementById('time');
