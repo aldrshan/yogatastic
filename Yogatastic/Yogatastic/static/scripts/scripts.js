8ae05389172d6cddfe58102e214ad02671d3785e
shnaldridge <shan.d.aldridge@gmail.com>
2023-03-22 10:06:22 -0400

    Basics work. Adding images and completing routines.

--- Yogatastic/Yogatastic/static/scripts/scripts.js
+++ Yogatastic/Yogatastic/static/scripts/scripts.js
@@ -1,13 +1,36 @@
-const yogaImages = [
-    'img/pose1.jpg',
-    'img/pose2.jpg',
-    'img/pose3.jpg',
-    // Add more images as needed
-];
+const yogaPoses = {
+    workout1: [
+        { name: "Child's Pose", image: "static/img/childs_pose.jpg" },
+        { name: "Cat-Cow Pose", image: "static/img/cat_cow_pose.jpg" },
+        // Add more poses for workout1
+    ],
+    workout2: [
+        { name: "Pose1", image: "static/img/pose1.jpg" },
+        { name: "Pose2", image: "static/img/pose2.jpg" },
+        // Add more poses for workout2
+    ],
+    workout3: [
+        { name: "Pose1", image: "static/img/pose1.jpg" },
+        { name: "Pose2", image: "static/img/pose2.jpg" },
+        // Add more poses for workout2
+    ],
+    workout4: [
+        { name: "Pose1", image: "static/img/pose1.jpg" },
+        { name: "Pose2", image: "static/img/pose2.jpg" },
+        // Add more poses for workout2
+    ],
+    workout5: [
+        { name: "Pose1", image: "static/img/pose1.jpg" },
+        { name: "Pose2", image: "static/img/pose2.jpg" },
+        // Add more poses for workout2
+    ],
+};
 
-const yogaImage = document.getElementById('yoga-image');
 const timerElement = document.getElementById('time');
+const yogaImage = document.getElementById('yoga-image');
+const workoutSelect = document.getElementById('workout-select');
 const startButton = document.getElementById('start-button');
+
 let currentPose = 0;
 let timer;
 
@@ -17,29 +40,36 @@ function startTimer(duration, display) {
         let minutes = parseInt(time / 60, 10);
         let seconds = parseInt(time % 60, 10);
 
-        minutes = minutes < 10 ? '0' + minutes : minutes
+        minutes = minutes < 10 ? '0' + minutes : minutes;
         seconds = seconds < 10 ? '0' + seconds : seconds;
 
         display.textContent = minutes + ':' + seconds;
 
         if (--time < 0) {
             clearInterval(timer);
-            nextPose();
+            setTimeout(() => {
+                nextPose();
+            }, 10000); // 10-second interval for transitions
         }
     }, 1000);
 }
 
 function nextPose() {
-    if (currentPose < yogaImages.length - 1) {
+    const selectedWorkout = workoutSelect.value;
+    const poses = yogaPoses[selectedWorkout];
+
+    if (currentPose < poses.length - 1) {
         currentPose++;
     } else {
         currentPose = 0;
     }
-    yogaImage.src = yogaImages[currentPose];
-    startTimer(30, timerElement); // Change 30 to the desired duration in seconds for each pose
+    yogaImage.src = poses[currentPose].image;
+    yogaImage.alt = poses[currentPose].name;
+    startTimer(60, timerElement); // Change to the desired duration in seconds for each pose
 }
 
 startButton.addEventListener('click', () => {
     clearInterval(timer);
+    currentPose = -1; // Set to -1 to start with the first pose when the button is clicked
     nextPose();
 });
